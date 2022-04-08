7a111a6f4cd6c2690c2de0e4802375c4531584a7
regoto <102773934+Regot0@users.noreply.github.com>
2022-04-08 00:31:04 +0000

    the entire game

--- main.py
+++ main.py
@@ -1,47 +1,43 @@
 from pprint import pprint as pprint
 import json
+from pdb import set_trace as st
 
 '''
 notes
 
 '''
-
+## load vars
+places = {}
+items = {}
+craft_items = {}
+npcs = {}
+entities = {}
+with open("data/places.json") as file:
+    places = json.load(file)
+with open("data/items.json") as file:
+    items = json.load(file)
+with open("data/craft_items.json") as file:
+    craft_items = json.load(file)
+with open("data/npcs.json") as file:
+    npcs = json.load(file)
+with open("data/entities.json") as file:
+    entities = json.load(file)
 
 state = {}
 
-run_mode="HUMAN"
-#run_mode="TEST"
+#run_mode = "HUMAN"
+run_mode="TEST"
 
-win = ['e','s','e','grab talkin_rattlesnake','craft nns', 'use nns']
-
-
-def intro():
-  print("The year is 1995, you were a renouned actor. But unfortunately you gambeled all the money you had in a casino, and because of that you couldn't pay back the the massive debt you owed. But it's all going to change...")
-
-  print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
-
-  print("Present day.")
-
-  print("It's 1997 it's been 2 years since you lost all your money. You wake up at 7:00am, thinking how to get your money back, due to huge press no one is hiring you for movies the only thing you're thinking of is how to get back on your feet.")
-  print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_")
-  print("you go for a walk thinking what to do, the first thing that comes to your mind is to get a new job but who wants to hire you?... you go buy some food, you buy: milk, bread, cheese, butter, soda, and some cookies. you tread back home but suddenly you bump into someone, it's a blond woman with long flowing hair she turned around and hands you a white card, you say what is this and who are you? she responds, it's a job, you interested? you take the card and look at it, the first thing it says is, go to a closed area and read the back... you look confused, and then you go home.")
-  print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
-
-  print("You get home and you look at the card, you go to the bathroom and you close everything the door, windows, you sit on the toilet and you look nervous, you look at the back and it says: a phone number: 702 007 2415... suddenly your phone starts ringing when you pick it up you hear the woman you met say staticly: stop the bomb... stop the spunnge. meet me at the cosino... you flinch, you put on your jacket and you get out of the apartment and you lock it and then youre on your way...")
-
-  print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
-
-
-if run_mode=="HUMAN":
-  while True:
-      do_intro = input('Want to do the intro? ').lower()
-      if do_intro == 'yes':
-          do_intro = True
-          intro()
-      else:
-          do_intro = False
-          break
-
+scripts = { 
+  'win': ['e', 'e', 'n', 'e', 'e', 'e', 'n', 'grab talkin_rattlesnake', 'craft nns', 's', 'w', 'w', 'w', 's', 'w', 'w','secret_place', 'use nns'],
+  'spunnge': ['e', 'e', 'n', 'e', 'e', 'e', 'n', 'grab talkin_rattlesnake', 'craft nns', 's', 'w', 'w', 'w', 's', 'w', 'w','secret_place', 'grab spunnge', 'use spunnge'],
+  'attack': ['grab rock', 'attack moe rock', 'attack moe rock'], 
+  'lose2': ['w',],
+  'map': ['e','n','s','e','n','n','s','e','s','n','e','s','n','e','n','s','e','w','s','n'],  # try to visit every room
+}
+#use_script='win'
+#use_script='map'
+use_script='attack'
 
 def get_command(question):
     '''
@@ -51,171 +47,171 @@ def get_command(question):
     answer = input(question).lower().strip()
     return answer.split()
 
-
-
-# master dictionary of items
-items = {}
-###
-craft_items = {}
-craft_items['spear'] = ['rock', 'rope', 'stick']
-craft_items['sheild'] = ['metalplate', 'leather', 'nails']
-craft_items['metalplate'] = ['scrap']
-craft_items['nns'] = ['talkin_rattlesnake']
-
-# master dictionary of npcs
-npcs = {}
-npcs['moe'] = "what are youse doing in my bar without buying a drink"
-npcs['Creepy_Bob'] = "myy names bob"
-npcs['liam'] = "the amongus boss is comming"
-npcs['crazy_guy'] = "I did see a talkin' rattlesnake"
-npcs['homer_simpson'] = "no beer and no tv make homer go crazy"
-npcs['talkin_rattlesnake'] = "They expirimented on me, tortured me because they knew that in my flesh held the key to creating the spoong and my flesh still holds the key to stop it. so I say to you stop the spoong and use my flesh."
-
+NYC = '''
+'''
 
 map = '''
-          fc
-          |
-          wp--tx
-          |
-     lc   |   
-     |    |   
-mt---fs---ph--
-     |
-     bs
+          fc          dv
+          |           |
+         wp--tx--gs--pb--ts
+          |    |  |   |
+     lc   |    cm Al  |
+     |    |           hd
+mt---fs---ph--ap
 
 
 '''
-# master dictionary of places
-places = {}
-with open("data/places.json") as file:
-  places = json.load(file)
-print(places)
 
 current_place = 'Moes Tavern'
 
 # items in players inventory
-itemsininv = [
-]
+itemsininv = []
 
 plot_moves = {
-  'diffused_bomb':0,
+    'diffused_bomb': 0,
+    'room': 0,
+    'lion_health': 0,
+    'PH': 10,
+    'dog': 0,
+    'winner': 0,
 }
 
-script_index=0  # keep track of which index we are on in the the script
+script_index = 0  # keep track of which index we are on in the the script
 
+
+def intro():
+    with open("data/intro.txt") as intro:
+        for line in intro:
+            print(line)
+
+
+if run_mode == "HUMAN":
+    while True:
+        do_intro = input('Want to do the intro? ').lower()
+        if do_intro[0] == 'y':
+            do_intro = True
+            intro()
+            break
+        else:
+            do_intro = False
+            break
 
 while True:
-  print('Your location: ', current_place)
+    print('Your location: ', current_place)
 
-  # tells you the description of the current place that you're in
-  print(places[current_place]['d'])
+    # tells you the description of the current place that you're in
+    print(places[current_place]['d'])
 
-  room_items = places[current_place]['room_items']
-  if room_items:
-      print('You see', ', '.join(room_items))
-      there_is_items = True
-  else:
-      there_is_items = False
+    room_items = places[current_place]['room_items']
+    if room_items:
+        print('You see', ', '.join(room_items))
+        there_is_items = True
+    else:
+        there_is_items = False
 
-  room_npcs=None
-  if 'room_npcs' in places[current_place]:
-    room_npcs = places[current_place]['room_npcs']
-  if room_npcs:
-    print('Also in this room:', room_npcs)
+    room_npcs = None
+    if 'room_npcs' in places[current_place]:
+        room_npcs = places[current_place]['room_npcs']
+    if room_npcs:
+        print('Also in this room:', room_npcs)
 
-  valid_moves = ', '.join(list(places[current_place]['moves'].keys()))
-  print("You can move: ", valid_moves)
-
-  if run_mode=="HUMAN":
-    move_raw = input("Your move: ").lower().strip()
-    print("---")
-  else:
-    move_raw = win[ script_index ]
-    script_index = script_index + 1
+    valid_moves = ', '.join(list(places[current_place]['moves'].keys()))
+    print("You can move: ", valid_moves)
 
 
-  # break up the command into parts
-  move_parts = move_raw.split(' ')
+    if run_mode == "TEST":
+          # did we run out of script commands? then switch to human mode
+        if ( len(scripts[use_script]) <= script_index ):
+          run_mode= "HUMAN"
+        else:
+          move_raw = scripts[use_script][script_index]
+          script_index = script_index + 1
+          print("TEST move: ", move_raw)
 
-  # get the verb which is the first part
-  move = move_parts[0]
+    if run_mode =="HUMAN":
+        move_raw = input("Your move: ").lower().strip()
+        print("---")
 
-  #print("  # move_raw",move_raw)
-  #print("  # move_parts",move_parts)
-  #print("  # move",move)
+    # break up the command into parts
+    move_parts = move_raw.split(' ')
 
-  obj = None
-  if len(move_parts) > 1:
-      obj = move_parts[1]
-  #print("You moved ", move)
+    # get the verb which is the first part
+    move = move_parts[0]
 
-  '''
+    #print("  # move_raw",move_raw)
+    #print("  # move_parts",move_parts)
+    #print("  # move",move)
+
+    obj = None
+    if len(move_parts) > 1:
+        obj = move_parts[1]
+    #print("You moved ", move)
+    '''
   Special plot moves BEFORE regular moves
   '''
 
+    # navigation move?
+    if move in ['e', 'w', 's', 'n']:
+
+        if move not in places[current_place]['moves']:
+            print("Cant move there")
+            continue
+        next_place = places[current_place]['moves'][move]
+        current_place = next_place
+
+    elif move in ['inv']:
+        print("Inventory")
+        print("---------")
+        print(itemsininv)
+
+    #elif move in ['recipes']:
+    #    print(craft_items)
+    #    print("---------")
+
+    elif move in ['map']:
+        print(map)
+
+    elif move in ['secret_place']:
+        #places[current_place] = 'place'
+        if current_place == 'Moes Tavern':
+          current_place = 'moes hidden room'
 
 
-  # navigation move?
-  if move in ['e', 'w', 's', 'n']:
+    elif move in ['craft']:
+        craft_obj = move_parts[1]
 
-      if move not in places[current_place]['moves']:
-          print("Cant move there")
-          continue
-      next_place = places[current_place]['moves'][move]
-      current_place = next_place
+        # use craft_items dictionary
+        # craft_items['spear']= ['rock','rope','stick']
 
-  elif move in ['inv']:
-      print("Inventory")
-      print("---------")
-      print(itemsininv)
+        if craft_obj not in craft_items.keys():
+            print("That item can not be crafted")
+        else:
 
-  #elif move in ['recipes']:
-  #    print(craft_items)
-  #    print("---------")
+            got_all_the_items = True
 
-  elif move in ['map']:
-      print(map)
+            items_needed = craft_items[craft_obj]
+            #['rock','rope','stick']
 
-  elif move in ['place']:
-    #places[current_place] = 'place'
-    current_place= move_parts[1]
-    
+            # check if you have the items needed to craft it
+            for item in items_needed:
+                if item not in itemsininv:
+                    print('dont have ', item)
+                    got_all_the_items = False
 
-  elif move in ['craft']:
-      craft_obj = move_parts[1]
+            # if you have all the items, remove all ingredients
+            if got_all_the_items == True:
+                for item in items_needed:
+                    if item in itemsininv:
+                        itemsininv.remove(item)
 
-      # use craft_items dictionary
-      # craft_items['spear']= ['rock','rope','stick']
+                # add the new crafted item into our inventory
+                itemsininv.append(craft_obj)
+                print('you crafted a ', craft_obj)
+            else:
+                print("Dont have all the items for that!")
 
-      if craft_obj not in craft_items.keys():
-          print("That item can not be crafted")
-      else:
-
-          got_all_the_items = True
-
-          items_needed = craft_items[craft_obj]
-          #['rock','rope','stick']
-
-          # check if you have the items needed to craft it
-          for item in items_needed:
-              if item not in itemsininv:
-                  print('dont have ', item)
-                  got_all_the_items = False
-
-          # if you have all the items, remove all ingredients
-          if got_all_the_items == True:
-              for item in items_needed:
-                  if item in itemsininv:
-                      itemsininv.remove(item)
-
-              # add the new crafted item into our inventory
-              itemsininv.append(craft_obj)
-              print('you crafted a ', craft_obj)
-          else:
-              print("Dont have all the items for that!")
-
-  elif move in ['help']:
-      print('''
+    elif move in ['help']:
+        print('''
 Commands:
 
 e w n s         Nagivation
@@ -229,58 +225,81 @@ craft (item)    craft items
   ''')
 #rickroll  rickroll
 
-  elif move in ['quit']:
-      exit()
+    elif move in ['quit']:
+        exit()
 
-  #elif move in ['secret_room']:
+    elif move in ['attack']:
+       entity_name = move_parts[1]
+       item_name = move_parts[2]
+       alive = entities[entity_name][ "life" ]
+       hits = entities[entity_name][ "hits" ]
+       hw = entities[entity_name][ "HW" ]
+       entity_details = entities[ entity_name ]
+       entity_damage = entities[entity_name][ "damage" ]
+       item_damage = items[item_name][ "damage" ]
+       if entity_name in places[current_place]['room_entities']:
+          if entities[entity_name]["life"] == 0:
+            if hits == 0: print(entity_details["responce"][0])
+            if hits == 1: print(entity_details["responce"][1])
+            if hits == 2: print(entity_details["responce"][2])
+          entity_details["health"] -= item_damage
+          entities[entity_name][ "hits" ] += 1
+          print(hits)
+          if hits >= hw: break
+          
+
+
+
+    #elif move in ['secret_room']:
     #set [current_place] 'place'
 
-  elif move in ['talk']:
-    npc = move_parts[1]
-    if npc in places[current_place] ['room_npcs']:
-      say =  npc + " says: " + npcs[ npc ]
-      print( say )
-      
-  elif move in ['grab']:
-      item = move_parts[1]
-      if item in places[current_place]['room_items']:
+    elif move in ['talk']:
+        npc = move_parts[1]
+        if npc in places[current_place]['room_npcs']:
+            say = npc + " says: " + npcs[npc]
+            print(say)
 
-          # add the item from inventory
-          itemsininv.append(item)
+    elif move in ['grab']:
+        item = move_parts[1]
+        if item in places[current_place]['room_items']:
 
-          # remove the item to the place
-          places[current_place]['room_items'].remove(item)
+            # add the item from inventory
+            itemsininv.append(item)
 
-      else:
-          print(item, "is not an item or is not here")
-          continue
+            # remove the item to the place
+            places[current_place]['room_items'].remove(item)
 
-  elif move in ['drop']:
-      item = move_parts[1]
-      if item in itemsininv:
-          # remove the item from inventory
-          itemsininv.remove(item)
+        else:
+            print(item, "is not an item or is not here")
+            continue
 
-          # add the item to the place
-          places[current_place]['room_items'].append(item)
-      else:
-          continue
-          print("Not a real move  -______-")
-      continue
+    elif move in ['drop']:
+        item = move_parts[1]
+        if item in itemsininv:
+            # remove the item from inventory
+            itemsininv.remove(item)
 
-  elif move in ['look']:
-      item = move_parts[1]
-      if item in places[current_place]['room_items'] or item in itemsininv:
-          print(items[item]['d'])
-      else:
-          print("you cant see it")
+            # add the item to the place
+            places[current_place]['room_items'].append(item)
+        else:
+            continue
+            print("Not a real move  -______-")
+        continue
 
-  #elif move in ['intro']:
-      do_intro = True
+    elif move in ['look']:
+        item = move_parts[1]
+        if item in places[current_place]['room_items'] or item in itemsininv:
+            print(items[item]['d'])
+        else:
+            print("you cant see it")
 
-  #
-  elif move in ['rick', 'rickroll', 'r', 'rr', 'RR']:
-      print('''
+
+    #elif move in ['intro']:
+        do_intro = True
+
+    #
+    elif move in ['rick', 'rickroll', 'r', 'rr', 'RR']:
+        print('''
 We're no strangers to love
 You know the rules and so do I
 A full commitment's what I'm thinking of
@@ -295,46 +314,93 @@ Never gonna say goodbye
 Never gonna tell a lie and hurt you
   ''')
 
-  elif move == "use":
-    item = move_parts[1]
-    if item=="nns":
-      if current_place=="Moes Tavern":
-        print("Ok you use the nns")
-        print("And it stops the bomb!")
+    elif move == "use":
+        item = move_parts[1]
+        if item == "nns":
+            if current_place == "moes hidden room":
+                print("Ok you use the nns")
+                print("And it stops the bomb!")
 
-        plot_moves['diffused_bomb']=1
+                plot_moves['diffused_bomb'] = 1
 
-      else:
-        print("You cant use that here")
+        pass
+        if item == "spear":
+            if current_place == "Lion Cage":
+              print("you stab the lion")
+              print("it doesn't die but is hurt")
+              print("your spear is now a damaged_spear")
+              print("it claws at you")
+
+              plot_moves['lion_health'] = 1
+              plot_moves['health'] = 1
+              itemsininv.remove("spear")
+              itemsininv.append("damaged_spear")
+
+        pass
+        if item == "damaged_spear":
+          if current_place == "Lion Cage":
+            if plot_moves['lion_health'] == 1:
+              print("your spearhead lodged in the lion killing it")
+              print("and you take it's teeth")
+              print("you see the door of the lion cage open")
+
+              plot_moves['lion_health'] = 2
+              itemsininv.append("lion teeth")
+            else:
+                print("You cant use that here")
+        pass
+        if item == "spunnge":
+              print("you are the stupidest person on earth!")
+              print("you killed us all!")
+              break
+
+
+        else:
+            print("You cant use that")
+
+
+
+    #elif move in ['resipe']:
+    #    print(resipe)
+
     else:
-      print("You cant use that")
-    pass
+        print("Not a real move")
+        print("-______-")
+        continue
 
-  #elif move in ['resipe']:
-  #    print(resipe)
+    '''
+    Special plot moves AFTER the move
+    '''
+    if move == 'grab' and obj == 'rock':
+        print("Under the rock you found a secretthing!")
+        itemsininv.append("secretthing")
 
-  else:
-      print("Not a real move")
-      print("-______-")
-      continue
+    if  move == 'e' and current_place == "Luxor Casino" and plot_moves['health'] == 0:
+      print("you manage to get out")
+      print("but not without getting scratched")
+
+      plot_moves['health'] = 2
+    '''
+  for later idc about it rn
+    if move == 'talk' and npc == 'dog' and current_place == "Allyway" and "dog" == 0:
+      "dog" == 1
+    '''
+
+    if move == 'w' and current_place == "Lion Cage" and plot_moves['health'] == 2:
+      print("you got eaten by the lion")
+      break
+
+    if  move == 'e' and current_place == "Luxor Casino" and plot_moves['health'] == 1:
+      print("you just barely manage to get out with your life")
+      print("you are extreamly close to death")
 
 
-  '''
-  Special plot moves AFTER the move
-  '''
-  if move == 'grab' and obj == 'rock':
-      print("Under the rock you found a secretthing!")
-      itemsininv.append("secretthing")
+    # WIN CONDITION
+    if plot_moves['diffused_bomb'] == 1:
+        # TODO: Write a fancy ending
+        print("congratulations you win")
+        print("you can keep playing")
+        "winner" == 1
 
-
-  # WIN CONDITION
-  if plot_moves['diffused_bomb'] == 1:
-    # TODO: Write a fancy ending
-    print("YOU WIN!!!")
-    print("GAME OVER")
-    break
-
-  print()
+    print()
 #print("dis is gonna be an inventory")r
-
-
